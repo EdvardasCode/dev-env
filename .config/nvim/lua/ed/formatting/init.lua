b4725b13b4fc1322bb509fdbf01f1e3a079f9f33
Edvardas <edvardas.kozlovas@gmail.com>
2026-05-11 17:03:02 +0100

    fix: replace deprecated conform lsp_fallback with lsp_format

--- .config/nvim/lua/ed/formatting/init.lua
+++ .config/nvim/lua/ed/formatting/init.lua
@@ -18,14 +18,14 @@ return {
       },
       format_on_save = {
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_format = "fallback",
       },
     })
 
     vim.keymap.set({ "n", "v" }, "<leader>f", function()
       require("conform").format({
         timeout_ms = 500,
-        lsp_fallback = true,
+        lsp_format = "fallback",
       })
     end)
   end,
